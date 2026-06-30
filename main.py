0174c2ee215f16f8522412d3a33b8a137446608a
tendryAxel <hei.tendry@gmail.com>
2026-06-30 13:22:09 +0300

    chore: profiling

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
+import random
 from quadtree import QuadTree, Vector2D
 
 
 def main():
     tree = QuadTree(10)
-    tree.add(Vector2D(1, 1), 1)
-    print(tree._grid_str())
-    tree.add(Vector2D(2, 1), 1)
-    print(tree._grid_str())
-    tree.add(Vector2D(1, 2), 1)
-    print(tree._grid_str())
-    tree.add(Vector2D(9, 9), 1)
-    print(tree._grid_str())
+    for i in range(100_000):
+        tree.add(Vector2D(random.random() * 10, random.random() * 10), 1)
 
 if __name__ == "__main__":
     main()
